908b7f9d233567dded368df37fb7724c3a636f15
gilson <gilson@olivers-air.home>
2020-10-19 17:10:13 +0100

    Top level componenet that contains all routes

--- src/components/main.js
+++ src/components/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route} from 'react-router-dom';
-import LandingPage from '../components/LandingPage/landingpage'
+import HomePage from './HomePage/homepage'
 import About from '../components/About/about'
 import Resume from '../components/Resume/resume'
 import Projects from '../components/Projects/projects'
@@ -10,7 +10,7 @@ import Contact from '../components/Contact/contact'
 
 const Main = () => (   
     <Switch>
-        <Route exact path="/" component={ LandingPage }/>
+        <Route exact path="/" component={ HomePage }/>
         <Route exact path="/about" component={ About }/>
         <Route exact path="/resume" component={ Resume }/>
         <Route exact path="/projects" component={ Projects }/>

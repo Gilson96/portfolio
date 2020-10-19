c6817fc3b1061ceb72bd5df733e49afe62093419
gilson <gilson@olivers-air.home>
2020-10-19 17:18:05 +0100

    Component that deals with the right column of resume

---
+++ src/components/Resume/Right_Column/Education/schoolTwo.js
@@ -0,0 +1,70 @@
+import React from 'react';
+
+const SchoolTwo = () => (
+    <li>
+        <div className="li_wrap">
+            <div className="date">
+                2020
+            </div>
+            <div className="info">
+                <p className="info_title">
+                    IT Technician
+                </p>
+                <p className="info_com">
+                    IT CAREER SWAP
+                </p>
+                <p className="info_count">
+                    <span>Theory Modules:</span>
+                    <br></br>      
+                    ● CompTIA IT Fundamentals
+                    <br></br>     
+                    ● CompTIA A + 1001&1002
+                    <br></br>
+                    ● CompTIA Network + N10‐007
+                    <br></br>      
+                    ● CompTIA Security + SY0‐501
+                    <br></br>     
+                    ● Cisco CCENT 100‐105
+                    <br></br>     
+                    ● MCSA Windows 10 70‐697
+                    <br></br>     
+                    ● MCSA Windows Server 2016
+                    <br></br> 
+                    <br></br> 
+
+                    <span>Practical Live Labs Training:</span>
+                    <br></br>      
+                    ● Implementing different boot methods
+                    <br></br>     
+                    ● Installing Operating Systems and applications
+                    <br></br>
+                    ● Disk Partitioning
+                    <br></br>      
+                    ● Using command line and windows system tools
+                    <br></br>     
+                    ● Using Mac OS and Linux with and the system tools
+                    <br></br>     
+                    ● Using Wi-Fi security protocols and authentication methods
+                    <br></br>     
+                    ● Working with tools used for malware detection, removal and prevention
+                    <br></br>     
+                    ● Securing workstations 
+                    <br></br>                     
+                    ● Troubleshooting Operating systems issues 
+                    <br></br>                    
+                    ● Troubleshooting PC security issues
+                    <br></br> 
+                    ● Implementing basic disaster prevention and recovery 
+                    <br></br> 
+                    ● Working with remote access technologies
+                    <br></br> 
+                    ● Using system restore
+                    <br></br>               
+                            
+                </p>
+            </div>
+        </div>
+    </li>
+);
+
+export default SchoolTwo;
---
+++ src/components/Resume/Right_Column/header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Header = () => (
+    <div className="header">
+        <div className="name_role">
+            <h2 className="name">
+                Gilson de Almeida
+            </h2>
+            <h4 className="role">
+                Full Stack Developper
+            </h4>
+        </div>
+        <hr className="resume-hr"/>
+        <div className="about">
+           <p>Enthusiastic professional eager to contribute to team success through hard work, attention to detail and organisation. I have always held a keen interest in IT and have recently undertaken a few online courses and a bootcamp. Motivated to learn, grow and excel in IT industry.</p> 
+        </div>
+
+    </div>
+);
+
+export default Header;

1fb3ec8961dfc5b00815f811181c24c647bc6484
Gilson Jeremias De Almeida <4jereg21@solent.ac.uk>
2020-11-30 15:25:31 +0000

    Updates

---
+++ precache-manifest.dd3b27cb85a32c90b6c0dce509564e43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eef108556df1bc80ebf3b510d93fbd4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ce476a9b2beb8e11ea3d",
+    "url": "/portfolio/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "1f2c61e5f866aa4d8971",
+    "url": "/portfolio/static/css/main.e94e16f7.chunk.css"
+  },
+  {
+    "revision": "ce476a9b2beb8e11ea3d",
+    "url": "/portfolio/static/js/2.9b051b75.chunk.js"
+  },
+  {
+    "revision": "7a9ffbcb0705b685a94df36082c8d5cd",
+    "url": "/portfolio/static/js/2.9b051b75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f2c61e5f866aa4d8971",
+    "url": "/portfolio/static/js/main.3b99cc46.chunk.js"
+  },
+  {
+    "revision": "272c84328c4e9a9d0bda",
+    "url": "/portfolio/static/js/runtime-main.6093c233.js"
+  }
+]);

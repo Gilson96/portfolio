944868be77f8ef2c3dfe2f9f2eea485454bb4a60
Gilson96 <grafael99@gmail.com>
2021-01-02 20:09:50 +0000

    Added a screenshot

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ProjectCard from '../Projects/ProjectCard';
-import Image from '../../Assets/marker01.png';
+import Image_One from '../../Assets/marker01.png';
+import Image_Two from '../../Assets/Screenshot.png';
 
 class Projects extends Component {
 
@@ -20,9 +21,16 @@ class Projects extends Component {
                         description="A fully operational React and Redux App with a laravel API and a Leaftlet.js map."
                         gitLink="https://github.com/tom-meyrick/iScream"
                         liveDemo="https://tom-meyrick.github.io/iScream/#/"
-                        image= {Image}
+                        image= {Image_One}
                     />
                     
+                    <ProjectCard
+                        title="CRUD APP"
+                        description="A basic crud app that is able to add, edit, delete a task.."
+                        gitLink="https://github.com/Gilson96/CRUD-App"
+                        liveDemo="https://gilson96.github.io/CRUD-App/"
+                        image= {Image_Two}
+                    />
                 </div>
 
             </section>

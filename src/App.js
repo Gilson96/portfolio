e1088d07804f9d58a98702e8e8d517be7d25aaf9
Gilson96 <grafael99@gmail.com>
2023-06-06 15:46:08 +0100

    Changed layout

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Navigator from './components/navigator';
-import Header_section from './components/header_section';
-import Skills from './components/skills';
-import Projects from './components/projects';
-import Footer from './components/footer';
-import Background from './components/backgrouns';
+import MainPage from './components/mainPage/mainPage';
+
 
 const App = () => (
 
-   <>
-      <div>
-         <Navigator />
-
-         <Header_section />
-
-         <Skills title={'Skills'} />
-
-         <Projects title={'Projects'} />
-
-         <Footer />
-      </div>
-   </>
+      <MainPage />
 );
 
 export default App;
